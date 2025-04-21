22d1f5443f4af4145827fbaadab9041bb5cae353
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-20 21:26:08 -0600

    Se crea el proyecto en java

---
+++ src/main/java/com/mycompany/proyecto_mipos/Proyecto_MIPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_mipos;
+
+/**
+ *
+ * @author LuisAmaya
+ */
+public class Proyecto_MIPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
