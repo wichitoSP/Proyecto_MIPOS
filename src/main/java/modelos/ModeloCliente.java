646256471f00c57e9ed4cc77bc3fed4b013b9974
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-21 15:03:30 -0600

    Se actualiza el proyecto

--- src/main/java/modelos/ModeloCliente.java
+++ src/main/java/modelos/ModeloCliente.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package modelos;
 
-/**
- *
- * @author LuisAmaya
- */
+package modelos;
 public class ModeloCliente {
+    
+    int idCliente;
+    String nombres;
+    String appaterno;
+    String appmaterno;
+
+    public ModeloCliente(int idCliente, String nombres, String appaterno, String appmaterno) {
+        this.idCliente = idCliente;
+        this.nombres = nombres;
+        this.appaterno = appaterno;
+        this.appmaterno = appmaterno;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getAppaterno() {
+        return appaterno;
+    }
+
+    public void setAppaterno(String appaterno) {
+        this.appaterno = appaterno;
+    }
+
+    public String getAppmaterno() {
+        return appmaterno;
+    }
+
+    public void setAppmaterno(String appmaterno) {
+        this.appmaterno = appmaterno;
+    }
+    
+    
+    
+    
     
 }
--- src/main/java/modelos/ModeloProducto.java
+++ src/main/java/modelos/ModeloProducto.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package modelos;
 
-/**
- *
- * @author LuisAmaya
- */
+package modelos;
 public class ModeloProducto {
+    
+    int idproducto;
+    String nombreProducto;
+    Double precioProducto;
+    int stockProducto;
+
+    public ModeloProducto(int idproducto, String nombreProducto, Double precioProducto, int stockProducto) {
+        this.idproducto = idproducto;
+        this.nombreProducto = nombreProducto;
+        this.precioProducto = precioProducto;
+        this.stockProducto = stockProducto;
+    }
+    
+    
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public Double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(Double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+
+    public int getStockProducto() {
+        return stockProducto;
+    }
+
+    public void setStockProducto(int stockProducto) {
+        this.stockProducto = stockProducto;
+    }
+    
+    
+    
+    
     
 }
